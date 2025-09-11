d2758140e1b4410b3c91ffb83608c1287ab4f081
Kjaiswal01 <kshitijj42@gmail.com>
2025-09-11 23:10:48 +0530

    Create Implement_Lower_&_Upper_BOUND.py

---
+++ Implement_Lower_&_Upper_BOUND.py
@@ -0,0 +1,50 @@
+#  Lower-Bound -------------------------------------------------
+
+nums = [1,1,1,3,3,3,5,6,7,7,7,9,9,12,12,12,13]
+n = len(nums)
+target = 7  
+lb = n
+low = 0
+high = n-1
+while low <= high:
+    mid = (low + high) // 2
+    if nums[mid] >= target:
+        lb = mid 
+        high = mid -1
+    else:
+        low = mid + 1  
+print(lb)     
+
+#  upper Bound -------------------------------------------------
+nums = [1,1,1,3,3,3,5,6,7,7,7,9,9,12,12,12,13]
+n = len(nums)
+target = 9
+ub = n
+low = 0
+high = n-1
+while low <= high:
+    mid = (low + high) // 2
+    if nums[mid] > target:
+        ub = mid 
+        high = mid -1
+    else:
+        low = mid + 1  
+print(ub)      
+
+
+
+# Leet code 35 - SEARCH INSERT POSITION ---------Same code as their lower bound -----------------
+nums = [1,1,1,3,3,3,5,6,7,7,7,9,9,12,12,12,13]
+n = len(nums)
+target = 2
+lb = n
+low = 0
+high = n-1
+while low <= high:
+    mid = (low + high) // 2
+    if nums[mid] >= target:
+        lb = mid 
+        high = mid -1
+    else:
+        low = mid + 1  
+print(lb) #--> 3
