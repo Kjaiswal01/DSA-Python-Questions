a1e4bc1f291d16331d4f05e32cc6d3614080e218
Kjaiswal01 <kshitijj42@gmail.com>
2025-09-14 22:49:41 +0530

    Update Factorial o n number.py

--- Factorial o n number.py
+++ Factorial o n number.py
@@ -1,18 +1,18 @@
-# time complexity -> o(n)
-# space complexity -> o(n)--> stack space
+# # time complexity -> o(n)
+# # space complexity -> o(n)--> stack space
 
-def fact(num):
-    if num==0 or num== 1:
-        return 1
-    return num*fact(num-1)
-print(fact(4))
+# def fact(num):
+#     if num==0 or num== 1:
+#         return 1
+#     return num*fact(num-1)
+# print(fact(4))
 
-class Solution:
-    def func(self, num):
-        if num == 1 or num == 0:
-            return 1
-        return num * self.func(num - 1)
+# class Solution:
+#     def func(self, num):
+#         if num == 1 or num == 0:
+#             return 1
+#         return num * self.func(num - 1)
 
-obj = Solution()
-print(obj.func(5))
+# obj = Solution()
+# print(obj.func(5))
 
